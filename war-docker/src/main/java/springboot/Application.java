59e070a72aea49d4a528be396bc0cd00c4b4c8f3
sontruong <truongdovinhson@gmail.com>
2019-10-19 16:28:14 +0700

    add demo war docker image

---
+++ war-docker/src/main/java/springboot/Application.java
@@ -0,0 +1,29 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application extends SpringBootServletInitializer {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.run(args);
+	}
+
+}
