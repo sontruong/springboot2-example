dee4fa00bb667869216648b7fde7a4ebc331efc2
sontruong <truongdovinhson@gmail.com>
2021-02-21 10:27:50 +0700

    add spring redis data deme

--- redis/src/main/java/com/one/demo/redis/DemoApplication.java
+++ redis/src/main/java/com/one/demo/redis/DemoApplication.java
@@ -11,10 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class DemoApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	
 }
