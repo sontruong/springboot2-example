12a33d33a28aa1204ff64544c6fe473a56143a47
sontruong <truongdovinhson@gmail.com>
2021-03-20 07:25:56 +0700

    update code

--- redis/src/main/java/com/one/demo/redis/database/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.one.demo.redis.database;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, String> {
-
-}
